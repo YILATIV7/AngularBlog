be83009ca0b343a8ec4588374018dfff72527577
Vitalii <vitalytyrenko7@gmail.com>
2024-09-07 18:06:22 +0300

    Introduced use of @ngrx/entity

--- src/app/models/comments.state.ts
+++
@@ -1,9 +0,0 @@
-import {Comment} from "./comment.model";
-
-export interface CommentsState {
-  isListLoading: boolean,
-  isListLoadingError: boolean,
-  comments: Comment[],
-  isCommentUploading: boolean,
-  isCommentUploadingError: boolean,
-}
